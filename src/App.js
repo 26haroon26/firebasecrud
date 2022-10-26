d9817f7db529a1704564c87612189aa85136baac
Muhammad Haroon <84071543+26haroon26@users.noreply.github.com>
2022-10-26 20:52:21 +0500

    Add files via upload
    uncomplete

--- src/App.js
+++ src/App.js
@@ -1,102 +1,40 @@
-// 1 : 36 minute
-import { useState, useEffect } from "react";
-import "./App.css";
-// import axios from "axios";
-import moment from "moment";
-import { initializeApp } from "firebase/app";
-import { getFirestore, collection, addDoc, getDocs } from "firebase/firestore";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCUnoboaWcVh3P2q9TMmc6yW8EtQC0XEuw",
-  authDomain: "socialmediaapp-d9873.firebaseapp.com",
-  projectId: "socialmediaapp-d9873",
-  storageBucket: "socialmediaapp-d9873.appspot.com",
-  messagingSenderId: "121456148400",
-  appId: "1:121456148400:web:087cebaa92781388666075",
-};
-
-const app = initializeApp(firebaseConfig);
-
-const db = getFirestore(app);
-
+import RightSide from './rightSide';
+import LeftSide from './leftSide';
+import Center from './center';
+import Navbar from './navbar';
+import { useEffect, useState } from 'react';
+import './App.css';
+import './toggle.css';
 function App() {
-  const [postData, setpostData] = useState("");
-  const [Posts, setPosts] = useState([]);
-  // const [loading, setloading] = useState(false);
-
+  const [lit, setlit] = useState(true);
+  const backColor = lit ? 'normal' : 'hard';
+  const textColor = lit ? 'light' : 'dark';
+  // const buttonColor = lit ? 'solid' : 'liquid';
+  const imgsrc = lit ? 'sun':'moon';
+  
   useEffect(() => {
-    const getPostData = async () => {
-      const querySnapshot = await getDocs(collection(db, "posts"));
-      querySnapshot.forEach((doc) => {
-        console.log(`${doc.id} => `, doc.data());
-        setPosts([...Posts,doc.data()])
-        getPostData();
-      });
-    };
-  }, []);
-
-  const SavePost = async (e) => {
-    e.preventDefault();
-    console.log("postData", postData);
-    try {
-      const docRef = await addDoc(collection(db, "posts"), {
-        text: postData,
-        time: new Date().getTime(),
-      });
-      console.log("Document written with ID: ", docRef.id);
-    } catch (e) {
-      console.error("Error adding document: ", e);
-    }
-  };
+    localStorage.setItem('class',`${lit}`) 
+   },)
+  let colorChanger = () => {
+    setlit(!lit)
+    localStorage.setItem('class',`${!lit}`)
+  }
+ 
+  
   return (
-    <div>
-      <form onSubmit={SavePost} className="form">
-        <textarea
-          className="input"
-          type="text"
-          placeholder="What's in your mind..."
-          onChange={(e) => {
-            setpostData(e.target.value);
-          }}
-        />
-        <button type="submit" className="button">
-          Post
-        </button>
-      </form>
-      {/* {loading ? "loading..." : ""} */}
-      <div className="body">
-        <div className="flex">
-          {Posts.map((eachPost) => (
-            <div className="post" key={eachPost?.time}>
-              <div className="postText">
-                {/* <a
-                  className="title"
-                  href={eachPost?.url}
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  {eachPost?.name}
-                </a>
-
-                <span>
-                  {moment(eachPost?.datePublished).format("Do MMMM, h:mm a")}
-                </span> */}
-
-                <h3 className="postDescr">{eachPost?.text}</h3>
-              </div>
-{/* 
-              <img
-                src={eachPost?.image?.thumbnail?.contentUrl
-                  .replace("&pid=News", "")
-                  .replace("pid=News&", "")
-                  .replace("pid=News", "")}
-                alt=""
-              /> */}
-            </div>
-          ))}
-        </div>
+    <>
+      <div className={`top ${textColor} ${backColor} `}>
+      <button className='btnChanger' onClick={colorChanger}><img className='sunMoon' src={`./images/${imgsrc}.png`} alt='aaaa' /></button>
+        <Navbar />
       </div>
-    </div>
+
+      <div className={`bottom  ${backColor} ${textColor}`}>
+        < RightSide />
+        < Center />
+        < LeftSide />
+      </div>
+
+    </>
   );
 }
 
---
+++ src/center.jsx
@@ -0,0 +1,100 @@
+import "./center.css";
+import "./toggle.css";
+import React, { useState } from "react";
+import Slider from './swiper';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faThumbsUp, faComment, faShare } from '@fortawesome/free-solid-svg-icons'
+
+function Post({profilePhoto, name, postDate, postText, postImage}) {
+
+  const [currentColor, setcurrentColor] = useState(true)
+   let colorChange = ()=>{
+     setcurrentColor(!currentColor)
+   }
+   let classValue  =  localStorage.getItem("class");
+ 
+  // let color;
+  // if( classValue === "true"){
+    // color="red"
+  // }else {
+  //   color="yellow"
+  // }
+  return (
+    <div >
+  <div className={`post ${classValue ==='true' ? 'yesPost':"noPost"}`} >
+{/* <div className={color}>check </div> */}
+    <div className='postHeader'>
+      <img className='profilePhoto' src={profilePhoto} alt="profile" />
+      <div>
+        {name} <br />
+        {postDate}
+      </div>
+    </div>
+    
+    <div className='postText'>
+      {postText}
+    </div>
+    <img className='postImage' src={postImage} alt="post" />
+
+    <hr />
+    <div className='postFooter'>
+      <div onClick={colorChange}> <FontAwesomeIcon className={currentColor? 'black':'yellow' 
+      // (classValue === 'true' ? )
+    }  icon={faThumbsUp} /> like </div>
+      <div > <FontAwesomeIcon icon={faComment} /> comment</div>
+      <div > <FontAwesomeIcon icon={faShare} /> share</div>
+    </div>
+    <hr />
+
+  </div>
+    </div>
+  );
+}
+function Center () {
+  // let classValue  =  localStorage.getItem("class");
+  return <div className='Center'>
+<div className="swiperSlider">
+< Slider />
+</div>
+
+    <Post
+      name="Arsalan"
+      profilePhoto="./images/img1.png"
+      postDate="13 Dec "
+      postImage="./images/postimage1.jpg"
+      postText="Alhamdulillah !!!
+      Finally the wait is over and By the grace of Almighty Allah we are proudly present
+      SAYLANI ASAN SADQA application.
+      SADQA now just one click away !
+      Download and rate as well !
+       feeling proud at Saylani Welfare International Trust.."
+    />
+    <Post
+      name="Malik"
+      profilePhoto="./images/profileimage1.jpg"
+      postDate="19 Jun "
+      postImage="./images/postimage.jpg"
+      postText="The guides area is designed to help developers learn to better interact with the date and time problem domain, and the Moment.js library."
+    />
+    <Post
+      name="Rehan"
+      profilePhoto="./images/profileimage2.jpg"
+      postDate="2 Aug "
+      postImage="./images/postimage2.jpg"
+      postText="The guides area is designed to help developers learn to better interact with the date and time problem domain, and the Moment.js library."
+    />
+    <Post
+      name="Saylani"
+      profilePhoto="./images/profileimage3.jpg"
+      postDate="23 May "
+      postImage="./images/postimage3.jpg"
+      postText="The guides area is designed to help developers learn to better interact with the date and time problem domain, and the Moment.js library."
+/>
+
+
+  </div>
+  
+}
+
+export default Center;
